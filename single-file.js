bc215dee10206fa3c21193ddc30a3fbb1a0a994b
tom-servo <kevin.convy@atsid.com>
2013-05-06 14:51:56 -0700

    Add configuration files for publishing on npm and optimizing with r.js.

---
+++ single-file.js
@@ -0,0 +1,8 @@
+({
+    baseUrl: "./src/main/javascript",
+    paths: {
+        "amd-plugins": "."
+    },
+    name: "amd-plugins/allpack",
+    out: "amd-plugins-1.0.0-min.js"
+})
---
+++ src/main/javascript/allpack.js
@@ -0,0 +1,7 @@
+define([
+    "./ioc",
+    "./jsonschema",
+    "./properties",
+    "./text"
+], function() {
+});
