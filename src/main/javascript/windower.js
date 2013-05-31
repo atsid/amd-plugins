79e87905797e4bf8f5465be12a96e188c63f2749
talkswithnumbers <cloudbees@talkswithnumbers.com>
2013-05-30 17:35:05 -0700

    added windower plugin

---
+++ src/main/javascript/windower.js
@@ -0,0 +1,252 @@
+/**
+ * This AMD plugin acts like a seed file coordinating multiple windows opened from a parent.
+ * This can be used for (a) postMessage calls, and (b) shared data storage using a private hash on the parent.
+ * See the very bottom for the public functions that are exposed by the plugin.
+ *
+ * The intent is to wrap up postMessage functionality behind a simple wrapper that deals with window listeners, etc.
+ * Also, the data setting/getting is done via postMessage as well, in order to ensure "clean" transfer.
+ * This means the data must be serializable. Part of the wrapper's activity then is to "hide" the data get/set
+ * messages from normal message traffic. Of course, if you setup your own event listeners you can inspect those "private"
+ * messages, but the intent isn't to keep the data secret, merely to provide a clean API for shared data storage.
+ *
+ * At the very least an array of children must be maintained on the parent, so postMessage can be delegated properly.
+ * Some browsers only allow postMessage to the parent, not to others in the child array. Therefore we'll delegate everything through topmost.
+ * See: http://blogs.msdn.com/b/thebeebs/archive/2011/12/21/postmessage-popups-and-ie.aspx
+ *
+ * Note that this automatically registers each window as a child, and hooks dom events to unregister on close.
+ * Children also self-register instead of topmost keeping a handle from window.open, because not all browsers return the child window instance.
+ * The topmost window will automatically close all children if it is unloaded, in order to guarantee synchronized state.
+ * Because children automatically register and unregister, they can be refreshed and will reconnect to the message bus.
+ *
+ * TODO: this only allows exact origin matches. it might be nice to allow config to be supplied that expands the origin matching.
+ */
+define(function () {
+
+    "use strict";
+
+    function getFirstOpener(win) {
+        if (win.opener) {
+            //prevent infinite recursion on page refresh
+            if (window.windowerName === win.opener.windowerName) {
+                return win;
+            }
+            return getFirstOpener(win.opener);
+        } else {
+            return win;
+        }
+    }
+
+
+    //these functions will be added to the parent window to wrap private shared objects
+    var parentFunctions = function () {
+
+            var children = {},
+                id = 0,
+                dataCache = {};
+
+            return {
+                addChild: function (child) {
+                    children[child.windowerName] = child;
+                },
+                removeChild: function (child) {
+                    delete children[child.windowerName];
+                },
+                iterateChildren: function (callback) {
+                    Object.keys(children).forEach(function (key) {
+                        callback.call(this, children[key]);
+                    });
+                },
+                getNewWindowName: function () {
+                    return "window-" + (++id);
+                },
+                getData: function (key) {
+                    return dataCache[key];
+                },
+                setData: function (key, data) {
+                    dataCache[key] = data;
+                }
+            };
+
+        },
+
+        topmost = getFirstOpener(window),
+        listener = null;
+
+    if (!topmost.windowerFunctions) {
+        topmost.windowerFunctions = parentFunctions();
+    }
+
+    //whatever window this is, we're going to register it. either we're a child or a parent, and need to be accounted for as such
+    //children get added to an array of children for message delegation
+    //the parent gets a special event listener just for message propagation
+    (function () {
+
+        //ensure that every window that loads this has a unique name for messaging
+        window.windowerName = topmost.windowerFunctions.getNewWindowName();
+
+        window.postMessageProxy = function (data) {
+            window.postMessage(data, window.location.origin);
+        };
+
+        if (window.windowerName !== topmost.windowerName) {
+
+            //we aren't the parent, so add as a child
+            topmost.windowerFunctions.addChild(window);
+
+            //if a child window gets refreshed, make sure to unhook it, or else it'll end up with duplicate messages later
+            window.addEventListener("unload", function () {
+                topmost.windowerFunctions.removeChild(this);
+            });
+
+        } else {
+
+            //if we're the parent, we want to re-broadcast the messages (unless they are our "private" data transfer messages)
+            window.addEventListener("message", function (e) {
+
+                var type = e.data._type,
+                    key = e.data.key,
+                    getter = e.data._getter,
+                    data, msg;
+
+                if (e.origin === window.location.origin) {
+
+                    if (type === "data-set-request") {
+
+                        data = e.data.data;
+                        topmost.windowerFunctions.setData(key, data);
+
+                    } else if (type === "data-get-request") {
+
+                        data = topmost.windowerFunctions.getData(key);
+                        msg = {
+                            key: key,
+                            data: data,
+                            _type: "data-get-response",
+                            _getter: getter
+                        };
+
+                        if (getter === window.windowerName) {
+                            window.postMessageProxy(msg); //possibly data sending to self (maybe this should bypass postMessage?)
+                        } else {
+                            topmost.windowerFunctions.iterateChildren(function (child) {
+                                if (getter === child.windowerName) {
+                                    child.postMessageProxy(msg);
+                                }
+                            });
+                        }
+
+                    } else {
+
+                        topmost.windowerFunctions.iterateChildren(function (child) {
+                            child.postMessageProxy(e.data);
+                        });
+
+                    }
+
+                }
+
+            }, false);
+
+            //and close all children when we close, because topmost is the master
+            window.addEventListener("unload", function () {
+                topmost.windowerFunctions.iterateChildren(function (child) {
+                    child.close();
+                });
+            });
+
+        }
+
+    }());
+
+    return {
+
+        /**
+         * Sends a message to any window in the collection managed by a topmost parent (except the sender).
+         */
+        send: function (message) {
+            topmost.postMessageProxy({
+                _sender: window.windowerName,
+                message: message
+            });
+        },
+
+        /**
+         * Sets up window event listening to get sent messages from other windows.
+         * Unwraps event data from "message" event to simplify passing of messages.
+         * Does not listen on messages from the same window.
+         * NOTE: only one listener is allowed at a time, which should be reasonable with this API.
+         * It would be difficult otherwise to manage unregistering the events.
+         * @param callback
+         */
+        listen: function (callback) {
+
+            if (listener) {
+                throw new Error(window.windowerName + " already has a listener - please use windower.unlisten() first");
+            }
+
+            listener = function (e) {
+                if (e.origin === window.location.origin && !e.data._type && (window.windowerName !== e.data._sender)) {
+                    callback.call(window, e.data.message);
+                }
+            };
+
+            window.addEventListener("message", listener);
+
+        },
+
+        /**
+         * Unregisters listening on the "message" events with a given callback.
+         * According to the postMessage spec, you must pass the same function that you registered with.
+         * Since we kept a copy of it (in the "listener" wrapper), all you need to do is call this method.
+         */
+        unlisten: function () {
+            if (listener) {
+                window.removeEventListener("message", listener);
+                listener = null;
+            }
+        },
+
+        /**
+         * Gets a data object by key. Data is passed to callback when retrieved.
+         * The callback is used because we're going to pass it through postMessage asynchronously.
+         * @param key
+         * @param callback
+         */
+        get: function (key, callback) {
+
+            var handler = function (e) {
+                if (e.origin === window.location.origin) {
+                    if (e.data._type === "data-get-response" && e.data.key === key) {
+                        window.removeEventListener("message", handler); //only want this once.
+                        callback(e.data.data);
+                    }
+                }
+            };
+
+            window.addEventListener("message", handler);
+
+            topmost.postMessageProxy({
+                key: key,
+                _type: "data-get-request",
+                _getter: window.windowerName
+            });
+
+        },
+
+        /**
+         * Sets a data object in shared cache, using a key.
+         * Sets using postMessage so there is no shared context.
+         * @param key
+         * @param data
+         */
+        set: function (key, data) {
+            topmost.postMessageProxy({
+                key: key,
+                data: data,
+                _type: "data-set-request",
+                _setter: window.windowerName
+            });
+        }
+
+    };
+});
---
+++ src/test/javascript/TestWindower.js
@@ -0,0 +1,187 @@
+require([
+    "plugins/windower"
+], function (
+    windower
+) {
+
+    "use strict";
+
+    var win;
+
+    /**
+     * These test methods utilize html/extended-window-*.html to perform cross-window communication.
+     * There is a little bit of message/callback management here to make sure we're capturing the right things
+     * and allowing the async test to proceed properly.
+     * Each test method has some corresponding action in the extended window, which is documented there.
+     */
+    AsyncTestCase("TestWindower", {
+
+        setUp: function () {
+            jstestdriver.plugins.async.CallbackPool.TIMEOUT = 2000;
+        },
+
+        tearDown: function () {
+            if (win) {
+                win.close();
+            }
+            windower.unlisten(); //TODO: use native methods to unlisten, so we aren't relying on the thing we're testing
+        },
+
+        //helper to open a window by name without having to re-type the path
+        openWindow: function (name) {
+            win = window.open("/test/src/test/javascript/html/" + name + ".html", name);
+        },
+
+        //sets up a message event, then executes a function that presumably will cause it to resolve
+        addMessageEventAndWait: function (queue, waitOperation) {
+            queue.call(function (callbacks) {
+                var callback = callbacks.add(function (e) {
+                    jstestdriver.console.log("got waiting message " + JSON.stringify(e.data));
+                    window.removeEventListener(callback);
+                });
+                window.addEventListener("message", callback);
+            });
+            waitOperation.call(this);
+        },
+
+        //helper to open a window and wait for a "message" event before allowing the test to proceed
+        openWindowAndWait: function (name, queue) {
+
+            this.addMessageEventAndWait(queue, function () {
+                this.openWindow(name);
+            });
+
+        },
+
+        //helper to add a callback and then listen on it with the windower
+        addListenerWithCallback: function (callbacks, handler, count) {
+
+            var callback = callbacks.add(handler, count || 1);
+
+            windower.listen(callback);
+        },
+
+        //the extended window is going to send a message when it loads.
+        //this will test that the message comes through the windower registration mechanisms properly.
+        testBasicMessaging: function (queue) {
+
+            var title = null;
+
+            queue.call(function (callbacks) {
+
+                this.addListenerWithCallback(callbacks, function (msg) {
+                    jstestdriver.console.log("got basic message: " + JSON.stringify(msg));
+                    title = msg.title;
+                });
+
+                this.openWindow("extended-window-basic");
+
+            });
+
+            queue.call(function () {
+                assertEquals("hi", title);
+            });
+
+        },
+
+        //this confirms that the loopback structure we have within the extended window works properly, verifying two-way communication
+        testLoopback: function (queue) {
+
+            var looped = false;
+
+            this.openWindowAndWait("extended-window-loopback", queue);
+
+            queue.call(function (callbacks) {
+
+                this.addListenerWithCallback(callbacks, function (msg) {
+                    jstestdriver.console.log("got loopback message in loopback test: " + JSON.stringify(msg));
+                    looped = msg.looped;
+                });
+
+                windower.send({"loopback": true});
+
+            });
+
+
+            queue.call(function () {
+                assertTrue(looped);
+            });
+
+        },
+
+
+        //test setting data in the extended window, and then retrieving it in the main window
+        testDataSetByExtendedAndGetByMain: function (queue) {
+
+            var dataValue;
+
+            this.openWindowAndWait("extended-window-set-ext", queue);
+
+            queue.call(function (callbacks) {
+
+                var callback = callbacks.add(function (data) {
+                    jstestdriver.console.log("got data " + JSON.stringify(data));
+                    dataValue = data.value;
+                });
+
+                windower.get("test-data", callback);
+
+            });
+
+            queue.call(function () {
+                assertEquals(12, dataValue);
+            });
+
+        },
+
+        //test the opposite - setting in the main window, then getting in the extended
+        testDataSetByMainAndGetByExtended: function (queue) {
+
+            var dataValue;
+
+            this.addMessageEventAndWait(queue, function () {
+                windower.set("test-data-2", {"value": 27});
+            });
+
+            this.openWindowAndWait("extended-window-set-main", queue);
+
+
+            queue.call(function (callbacks) {
+
+                this.addListenerWithCallback(callbacks, function (msg) {
+                    jstestdriver.console.log("got data message from extended window: " + JSON.stringify(msg));
+                    dataValue = msg.value;
+                });
+
+                windower.send({"op": "get it"});
+
+            });
+
+            queue.call(function () {
+                assertEquals(27, dataValue);
+            });
+
+        },
+
+        testUnlisten: function (queue) {
+
+            windower.listen(function () {});
+
+            try {
+                windower.listen(function () {});
+                assertTrue(false);
+            } catch (e) {
+                jstestdriver.console.log("Got expected multiple-listeners error");
+                assertTrue(true);
+            }
+
+            //once we unlisten, we can listen again
+            windower.unlisten();
+            windower.listen(function () {});
+
+        }
+
+
+    });
+
+});
