9d967a4eb0e391c5103cd36717e9a001624032ea
Nathan Evans <github@talkswithnumbers.com>
2015-07-23 10:35:40 -0700

    #2 - turned uglify back on

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,6 @@ module.exports = function (grunt) {
         requirejs: {
             compile: {
                 options: {
-                    optimize: 'none',
                     baseUrl: './js',
                     paths: {
                         plugins: '.'
